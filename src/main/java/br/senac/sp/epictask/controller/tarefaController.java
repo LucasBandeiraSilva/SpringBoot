adc419ce0120c9f9392f4eda7b9894d69ae4fddc
LUCAS BANDEIRA DA SILVA <lucas.bsilva85@senacsp.edu.br>
2023-05-26 21:48:31 -0300

    autenticação

--- src/main/java/br/senac/sp/epictask/controller/tarefaController.java
+++ src/main/java/br/senac/sp/epictask/controller/tarefaController.java
@@ -2,8 +2,11 @@ package br.senac.sp.epictask.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.senac.sp.epictask.data.tarefaRepository;
 import br.senac.sp.epictask.model.Tarefa;
@@ -15,7 +18,9 @@ public class tarefaController {
     tarefaRepository repository;
 
     @GetMapping("/tarefas")
-    public String  tarefas(){
+    public String  tarefas(Model model){
+        var tarefas = repository.findAll();
+        model.addAttribute("tarefas", tarefas);
         return "tarefas";
     }
 
@@ -27,6 +32,18 @@ public class tarefaController {
     @PostMapping("/tarefas")
         public String cadastrar(Tarefa tarefa){
             repository.save(tarefa);
+            return "redirect:/tarefas";
+        }
+
+        @GetMapping("/")
+        public String home(){
+            return "redirect:/tarefas";
+        }
+
+        @DeleteMapping("/tarefas")
+        public String apagar(long id, RedirectAttributes redirect){
+            repository.deleteById(id);
+            redirect.addFlashAttribute("mensagem", "tarefa apagada com sucesso");
             return "redirect:/tarefas";
         }
     }
